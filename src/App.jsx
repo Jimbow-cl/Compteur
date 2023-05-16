e8498d501516d1863178e1af74d8103b04418535
Jimbow-cl <cart-lamy.jimmy@live.fr>
2023-05-16 14:12:59 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useReducer } from 'react';
+import './App.css'
+
+function App() {
+
+  const [count, updateCount] = useReducer(countReducer, 0);
+
+  function countReducer(state, action) {
+    if (action === "increment") {
+      return state + 1;
+    } else if (action === "decrement") {
+      return state - 1;
+    } else if (action === "double") {
+      return state * 2;
+    } else if (action === "puissance") {
+      return state * state;
+    } else if (action === "divide") {
+      return state / 2;
+    } else if (action === "reset") {
+      return state = 0;
+    }
+  }
+  return (
+    <div>
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() =>
+          updateCount("increment")}>+ 1</button>
+        <button onClick={() =>
+          updateCount("decrement")}>- 1</button>
+        <button onClick={() =>
+          updateCount("double")}>x 2</button>
+        <button onClick={() =>
+          updateCount("puissance")}>count x count</button>
+        <button onClick={() =>
+          updateCount("divide")}>/ 2</button>
+        <button onClick={() =>
+          updateCount("reset")}>Reset</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
